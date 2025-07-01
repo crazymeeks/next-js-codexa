2a86089be1211c461c504dac9d5f3743a7bdf738
Jeff Claud <jeff@leadingedgeweb.com.au>
2025-07-01 20:28:48 +0800

    create cartSlice

--- src/redux/features/cart/cartSlice.js
+++ src/redux/features/cart/cartSlice.js
@@ -3,12 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const cartSlice = createSlice({
   name: 'cart',
+  
   initialState: {
-    addedToCart: false
+    counter: 0
   },
+
   reducers: {
-    addToCart: (state) => {
-      state.addedToCart = true;
-    },
+    
+    handleIncrease: (state) => {},
+
+    handleDecrease: (state) => {},
   }
 });
