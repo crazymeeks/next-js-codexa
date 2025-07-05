788401c90bbbd4c435d87c0041f2a8613cc53649
Jeff Claud <jeff@leadingedgeweb.com.au>
2025-07-05 09:39:13 +0800

    design product list

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,8 +1,13 @@
 'use client';
 
-import ProductModal from "./ProductModal";
 import { useDispatch, useSelector } from "react-redux";
 import { cartSlice } from "@/redux/features/cart/cartSlice";
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card";
+import { Button } from "../ui/button";
 
 const ProductList = () => {
 
@@ -10,32 +15,77 @@ const ProductList = () => {
 
   const dispatch = useDispatch();
 
-  const handleIncrease = () => {
-    dispatch(cartSlice.actions.handleIncrease());
-  };
-
-  const handleDecrease = () => {
-    dispatch(cartSlice.actions.handleDecrease());
-  };
-
   return (
-    <>
-      <div>Count: {cart.counter}</div>
-      <div className="flex gap-2">
-        <button
-          onClick={handleIncrease}
-          className="cursor-pointer bg-blue-400 p-2">
-          Increase
-        </button>
-        <button
-          onClick={handleDecrease}
-          className="cursor-pointer bg-red-400 p-2">
-          Decrease
-        </button>
-      </div>
-
-      <ProductModal/>
-    </>
+    <div className="grid md:grid-cols-4 gap-2">
+        <Card>
+            <CardContent>
+                <div className="w-full aspect-[4/3] relative rounded-md overflow-hidden">
+                  <Image
+                    src="/burger.webp"
+                    alt="Burger"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+                <div className="flex justify-between mt-2">
+                  <h3 className="font-bold">Burger</h3>
+                  <p>PHP300.00</p>
+                </div>
+                <Button className="w-full font-bold">ADD TO CART</Button>
+            </CardContent>
+        </Card>
+        <Card>
+            <CardContent>
+                <div className="w-full aspect-[4/3] relative rounded-md overflow-hidden">
+                  <Image
+                    src="/fried-chicken.webp"
+                    alt="Fried chicken"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+                <div className="flex justify-between mt-2">
+                  <h3 className="font-bold">Fried Chicken</h3>
+                  <p>PHP250.00</p>
+                </div>
+                <Button className="w-full font-bold">ADD TO CART</Button>
+            </CardContent>
+        </Card>
+        <Card>
+            <CardContent>
+                <div className="w-full aspect-[4/3] relative rounded-md overflow-hidden">
+                  <Image
+                    src="/fries.png"
+                    alt="Fries"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+                <div className="flex justify-between mt-2">
+                  <h3 className="font-bold">Fries</h3>
+                  <p>PHP60.00</p>
+                </div>
+                <Button className="w-full font-bold">ADD TO CART</Button>
+            </CardContent>
+        </Card>
+        <Card>
+            <CardContent>
+                <div className="w-full aspect-[4/3] relative rounded-md overflow-hidden">
+                  <Image
+                    src="/turon.jpg"
+                    alt="Turon"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+                <div className="flex justify-between mt-2">
+                  <h3 className="font-bold">Turon</h3>
+                  <p>PHP15.00</p>
+                </div>
+                <Button className="w-full font-bold">ADD TO CART</Button>
+            </CardContent>
+        </Card>
+    </div>
   );
 };
 
