7f4805d9f57cc29b362451d80047e207c2d54474
Jeff Claud <jeff@leadingedgeweb.com.au>
2025-07-05 17:21:56 +0800

    implement productSlice and add to cart feature

--- src/redux/features/cart/cartSlice.js
+++ src/redux/features/cart/cartSlice.js
@@ -3,19 +3,33 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const cartSlice = createSlice({
   name: 'cart',
-
   initialState: {
-    counter: 0
+    items: [
+      // {
+      //   id: null,
+      //   product_id: null,
+      //   image: null,
+      //   name: null,
+      //   quantity: 0,
+      //   price: 0
+      // },
+    ],
+    grand_total: 0,
+    overall_quantity: 0,
   },
-
   reducers: {
-    
-    handleIncrease: (state) => {
-      state.counter++;
+    addToCart: (state, action) => {
+      console.log("action: ", action);
+      state.items.push({
+        ...action.payload,
+        product_id: action.payload.id,
+        quantity: 1,
+        id: Math.random(),
+      });
+
+      state.overall_quantity += 1;
     },
 
-    handleDecrease: (state) => {
-      state.counter--;
-    },
+    deleteCartItem: (state, action) => {},
   }
 });
